7efcf20efe2a352bf7366aefb2e653b6d27e6238
Brendan Cordova <brendan.cordova@daveramsey.com>
2024-03-05 16:51:55 -0600

    playing with more jest methods

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,4 +1,21 @@
+export type stringInfo = {
+  lowerCase: string;
+  upperCase: string;
+  characters: string[];
+  length: number;
+  extraInfo: Object | undefined;
+};
 
-export function toUpperCase(arg: string){
-    return arg.toUpperCase()
+export function toUpperCase(arg: string) {
+  return arg.toUpperCase();
+}
+
+export function getStringInfo(arg: string): stringInfo {
+  return {
+    lowerCase: arg.toLowerCase(),
+    upperCase: arg.toUpperCase(),
+    characters: Array.from(arg), //create array based on arg
+    length: arg.length,
+    extraInfo: {},
+  };
 }
--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -1,11 +1,34 @@
-import { toUpperCase } from "../app/utils"
+import { getStringInfo, toUpperCase } from "../app/utils";
 
+describe("utils test suite", () => {
+  it("should return uppercase string", () => {
+    //arrange:
+    const sut = toUpperCase; //system under test
+    const expected = "ABC";
 
-describe('utils test suite', () => {
-    
-    test('should return uppercase', () => {
-        const result = toUpperCase('abc')
-        expect(result).toBe('ABC')
-    })
+    //act:
+    const actual = sut("abc");
 
-})
+    //assertion:
+    expect(actual).toBe(expected);
+  });
+
+  it("should return info for a valid string", () => {
+    const actual = getStringInfo("Example-Test");
+
+    expect(actual.lowerCase).toBe("example-test");
+    expect(actual.extraInfo).toEqual({});
+
+    //two below are the same
+    expect(actual.characters.length).toBe(12);
+    expect(actual.characters).toHaveLength(12);
+
+    expect(actual.characters).toContain<string>("E");
+    expect(actual.extraInfo).toEqual(
+      expect.arrayContaining(["E", "x", "a", "m", "p", "l", "e"])
+    );
+
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeTruthy();
+  });
+});
